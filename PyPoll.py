ff12e833caf091b43bf7ae1a140ceeb4c0062507
tpankratz1 <96349572+tpankratz1@users.noreply.github.com>
2022-01-12 23:31:07 -0600

    Adding the election audi code.

--- PyPoll.py
+++ PyPoll.py
@@ -1,10 +1,3 @@
-# # The data we need to retrieve.
-# # 1. The total number of votes cast
-# # 2. A complete list of candidates who received votes
-# # 3. The percentage of votes each candidate won
-# # 4. The total number of votes each candidate won
-# # 5. The winner of the election based on popular vote.
-
 # Add our dependencies.
 import csv
 import os
@@ -12,73 +5,55 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-
-# 1. Initialize a total vote counter.
+# Initialize a total vote counter.
 total_votes = 0
-
-# Candidate Options
+# Candidate options and candidate votes
 candidate_options = []
-
-# Declare the empty dictionary.
 candidate_votes = {}
-
-# Winning Candidate and Winning Count Tracker
+# Track the winning candidate, vote count, and percentage.
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
-
-# Open the election results and read the file
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-
     # Read the header row.
     headers = next(file_reader)
-
-# Print each row in the CSV file.
+    # Print each row in the CSV file.
     for row in file_reader:
         # Add to the total vote count.
         total_votes += 1
-
-        # Print the candidate name from each row.
+        # Get the candidate name from each row.
         candidate_name = row[2]
-
-        # If the candidate does not match any existing candidate...
+        # If the candidate does not match any existing candidate add it the
+        # the candidate list.
         if candidate_name not in candidate_options:
-           # Add the candidate name to the candidate list.
+            # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
-
-            # Begin tracking that candidate's vote count.
+            # And begin tracking that candidate's voter count.
             candidate_votes[candidate_name] = 0
-
-        # Add a vote to that candidate's count.
+        # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
 
-# Determine the percentage of votes for each candidate by looping through the counts.
-# Iterate through the candidate list.
 for candidate_name in candidate_votes:
-    # Retrieve vote count of a candidate.
+    # Retrieve vote count and percentage.
     votes = candidate_votes[candidate_name]
-    # Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
-
-    #  To do: print out each candidate's name, vote count, and percentage of
-    # votes to the terminal.
+    # Print each candidate, their voter count, and percentage to the
+    # terminal.
     print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning vote count and candidate
-    # Determine if the votes is greater than the winning count.
+    # Determine winning vote count, winning percentage, and candidate.
     if (votes > winning_count) and (vote_percentage > winning_percentage):
-         # If true then set winning_count = votes and winning_percent =
-         # vote_percentage.
-         winning_count = votes
-         winning_percentage = vote_percentage
-         # And, set the winning_candidate equal to the candidate's name.
-         winning_candidate = candidate_name
-
+        winning_count = votes
+        winning_candidate = candidate_name
+        winning_percentage = vote_percentage
+# Print the winning candidates' results to the terminal.
 winning_candidate_summary = (
     f"-------------------------\n"
     f"Winner: {winning_candidate}\n"
     f"Winning Vote Count: {winning_count:,}\n"
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
+
 print(winning_candidate_summary)
